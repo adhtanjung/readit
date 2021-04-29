560e41521a48b7d45f47053f05ac361152bc52a0
adhtanjung <adhtanjung@gmail.com>
2021-04-30 01:05:12 +0700

    login,register

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,5 +1,6 @@
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import { User } from "./entities/User";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
 export default {
@@ -7,7 +8,7 @@ export default {
 		path: path.join(__dirname, "./migrations"), // path to the folder with migrations
 		pattern: /^[\w-]+\d+\.[tj]s$/,
 	},
-	entities: [Post],
+	entities: [Post, User],
 	dbName: "readit",
 	type: "postgresql",
 	debug: !__prod__,
